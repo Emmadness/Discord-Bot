c03819e3836e2c6111161d22b364cc102988d087
Alberto Moncayo <moncayoalberto@proton.me>
2025-09-01 22:20:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const lastEmbeds = new Map();
 
 client.once('ready', async () => {
   console.log(`✅ Bot conectado como ${client.user.tag}`);
-  client.user.setActivity('a TruckersMP', { type: 0 });
+  client.user.setActivity('Gestionando la VTC', { type: 3 });
   await registerSlashCommands();
 });
 
@@ -185,5 +185,6 @@ client.on('interactionCreate', async interaction => {
 });
 
 client.login(process.env.DISCORD_TOKEN); // ELIMINAR DE LA LINEA DE ABAJO POR SI NO FUNCIONA
+
 
 
